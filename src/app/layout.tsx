4df5697d74128fd0155af58fd56ee3ece60ab960
Omar Zunic <contact@omarzunic.com>
2024-09-11 01:01:08 +0200

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import 'leaflet/dist/leaflet.css';
 import type {ReactNode} from "react";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <Script
+	    defer
+	    data-domain="livno.omarzunic.com"
+	    src="https://analytics.omarzunic.com/js/script.js"
+    />
       <body className={inter.className}>{children}</body>
     </html>
   );
